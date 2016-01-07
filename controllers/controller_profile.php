5ac1d61cb6d19a99a94f27efe05875c061ebb77d
eleonoria96@gmail.com <eleonoria96@gmail.com>
2016-01-07 18:05:05 +0200

    smth went wrong

---
+++ controllers/controller_profile.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Eleonora
+ * Date: 07.01.2016
+ * Time: 17:27
+ */
+class profile
+{
+
+}
---
+++ models/Model_Profile.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Eleonora
+ * Date: 07.01.2016
+ * Time: 17:28
+ */
+class Model_Profile
+{
+
+}
